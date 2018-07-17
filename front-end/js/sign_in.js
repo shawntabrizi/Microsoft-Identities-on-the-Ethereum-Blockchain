ccf87f21d874edb4080506927f8db569967a1cfb
Shawn Tabrizi <shawntabrizi@gmail.com>
2018-07-16 17:37:49 -0700

    Getting the sign in page started

---
+++ front-end/js/sign_in.js
@@ -0,0 +1,40 @@
+function sign_in() {
+    var userAgentApplication = new Msal.UserAgentApplication('79d908c3-6cc1-40c6-bbf1-9f7140e927fb', null, function (errorDes, token, error, tokenType, instance) {
+        // this callback is called after loginRedirect OR acquireTokenRedirect. It's not used with loginPopup,  acquireTokenPopup.
+        if (error) {
+            console.log(error + ": " + errorDes);
+        }
+        else
+            console.log("Token type = " + tokenType);
+
+    })
+
+    userAgentApplication.loginPopup(["user.read"]).then(function (token) {
+        var user = userAgentApplication.getUser();
+        if (user) {
+            console.log("signed in sucessfully");
+
+            // get an access token
+            userAgentApplication.acquireTokenSilent(["user.read"]).then(function (token) {
+                console.log("Success acquiring access token");
+            }, function (error) {
+                // interaction required
+                if (error.indexOf("interaction_required" != -1)) {
+                    userAgentApplication.acquireTokenPopup(["user.read"]).then(function (token) {
+                        console.log("Success acquiring access token");
+                    }, function (error) {
+                        console.log("Failure acquiring token: " + error);
+                    });
+                }
+            });
+        } else {
+            console.log("signed in failure");
+        }
+    }, function (error) {
+        console.log("error: " + error);
+    });
+}
+
+
+var sign_in_button = document.getElementById("sign_in_button")
+sign_in_button.addEventListener("click", sign_in);
