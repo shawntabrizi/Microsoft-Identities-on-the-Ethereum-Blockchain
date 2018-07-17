0125b45d87b84d1379c1b0ba0749d81627534104
Shawn Tabrizi <shawntabrizi@gmail.com>
2018-07-17 15:18:05 -0700

    Adding Sign Message Functionality

---
+++ front-end/js/sign_message.js
@@ -0,0 +1,45 @@
+window.addEventListener('load', function () {
+    if (typeof web3 !== 'undefined') {
+        console.log('Web3 Detected! ' + web3.currentProvider.constructor.name)
+        window.web3 = new Web3(web3.currentProvider);
+        afterLoad();
+    } else {
+        console.log('No Web3 Detected... please install Metamask')
+        //You need Metamask to provide account info, so Infura won't do!
+        //window.web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/<APIKEY>"));
+    }
+})
+
+const promisify = (inner) =>
+    new Promise((resolve, reject) =>
+        inner((err, res) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        })
+    );
+
+async function sign_message(message) {
+    var hash = web3.sha3(message)
+    var signed_message = promisify(cb => web3.personal.sign(hash, web3.eth.defaultAccount, cb))
+    return signed_message
+}
+
+async function sign_token() {
+    var jwt_raw = document.getElementById("jwt_raw").innerText
+    var signed_message = await sign_message(jwt_raw)
+    document.getElementById("signed_message").innerText = signed_message
+}
+
+function add_address() {
+    document.getElementById("eth_address").innerText = web3.eth.defaultAccount
+}
+
+function afterLoad() {
+    add_address()
+}
+
+var sign_message_button = document.getElementById("sign_message_button")
+sign_message_button.addEventListener("click", sign_token);
