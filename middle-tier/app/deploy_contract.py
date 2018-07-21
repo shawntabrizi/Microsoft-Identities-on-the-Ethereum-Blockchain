409158c6d34cc30c88f3a6bdc9813afd8f871b70
fernandolobato <lobato.meeser.fernando@hotmail.com>
2018-07-21 10:30:45 -0700

    refactor

--- middle-tier/app/deploy_contract.py
+++ middle-tier/app/deploy_contract.py
@@ -4,9 +4,12 @@ from web3 import Web3, HTTPProvider
 # we'll use ConciseContract to interact with our specific instance of the contract
 from web3.contract import ConciseContract
 
-def deploy_contract():
+def deploy_contract(
+    network='http://localhost:8545',
+    contractSource='../../identity-smart-contract/IdentityStore_Sample.sol'):
+    
     # open a connection to the local ethereum node
-    http_provider = HTTPProvider('http://localhost:8545')
+    http_provider = HTTPProvider(network)
     eth_provider = Web3(http_provider).eth
 
     default_account = eth_provider.accounts[0]
@@ -16,7 +19,7 @@ def deploy_contract():
     }
 
     #load solditiy code
-    with open('../../identity-smart-contract/IdentityStore_Sample.sol') as file:
+    with open(contractSource) as file:
         source_code = file.readlines()
 
     #compile contract
@@ -45,14 +48,12 @@ def deploy_contract():
     transaction_receipt = eth_provider.getTransactionReceipt(transaction_hash)
     contract_address = transaction_receipt['contractAddress']
 
-    contract_instance = eth_provider.contract(
-    abi=contract_abi,
-    address=contract_address,
     # when a contract instance is converted to python, we call the native solidity
     # functions like: contract_instance.call().someFunctionHere()
     # the .call() notation becomes repetitive so we can pass in ConciseContract as our
     # parent class, allowing us to make calls like: contract_instance.someFunctionHere()
-    ContractFactoryClass=ConciseContract,
-)
-
-
+    contract_instance = eth_provider.contract(
+        abi=contract_abi,
+        address=contract_address,
+        ContractFactoryClass=ConciseContract,
+    )
