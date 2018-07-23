0cceff2d540bfa3030b4452743f09ef80974977f
Brandon Trinh <brtrinh@microsoft.com>
2018-07-23 15:26:00 -0700

    update hash function

--- identity-smart-contract/migrations/1_initial_migration.js
+++ identity-smart-contract/migrations/1_initial_migration.js
@@ -5,13 +5,13 @@ var Migrations = artifacts.require("./Migrations.sol");
 var Identity = artifacts.require("IdentityStore");
 
 module.exports = function(deployer, network) {
-  const config = TruffleConfig.networks[network];
+  // const config = TruffleConfig.networks[network];
 
-  if(config.password) {
-    const web3 = new Web3(new Web3.providers.HttpProvider('http://' + config.host + ':' + config.port));
-    console.log('>> Unlocking account ' + config.address);
-    web3.personal.unlockAccount(config.address, config.password(), 36000);
-  }
+  // if(config.password) {
+  //   const web3 = new Web3(new Web3.providers.HttpProvider('http://' + config.host + ':' + config.port));
+  //   console.log('>> Unlocking account ' + config.address);
+  //   web3.personal.unlockAccount(config.address, config.password(), 36000);
+  // }
 
   deployer.deploy(Migrations);
 
