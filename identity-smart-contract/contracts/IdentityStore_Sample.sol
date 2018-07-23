ec60b2047e387c1a3c6e9f5dc9ab14196c4f1771
fernandolobato <lobato.meeser.fernando@hotmail.com>
2018-07-23 14:06:06 -0700

    changes to contract

--- identity-smart-contract/contracts/IdentityStore_Sample.sol
+++ identity-smart-contract/contracts/IdentityStore_Sample.sol
@@ -5,22 +5,40 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 contract IdentityStore is Ownable {
 
     struct User {
-        string tenantId;
+        bytes32 tenantHash;
         uint256 timestamp;
     }
 
-    mapping(address => User) public tenantMapping;
+    mapping(address => User) private tenantAddressMapping;
+    mapping(bytes32 => address) private tenantHashMapping; 
 
-    mapping(bytes32 => address) public tenantHashMapping; 
-    
     function setTenant(
         bytes32 _tenantHash,
         address _userAddress,
-        string _tenantId) onlyOwner public {
+        uint256 _timestamp) onlyOwner public {
         
-        User memory newUser = User(_tenantId, block.timestamp);
+        require(!userAddressExists(_userAddress));
+        require(!userTenantHashExists(_tenantHash));
 
-        tenantMapping[_userAddress] = newUser;
+        User memory newUser = User(_tenantHash, _timestamp);
+        
+        tenantAddressMapping[_userAddress] = newUser;
         tenantHashMapping[_tenantHash] = _userAddress;
-    }   
+    }
+
+
+    function userAddressExists(address userAddress) view public returns(bool) {       
+        if(tenantAddressMapping[userAddress].timestamp == 0) {
+            return false;
+        }
+        return true;
+    }
+
+    function userTenantHashExists(bytes32 tenantHash) view public returns(bool){
+        if(tenantHashMapping[tenantHash] == 0) {
+            return false;
+        }
+
+        return true;
+    }
 }
