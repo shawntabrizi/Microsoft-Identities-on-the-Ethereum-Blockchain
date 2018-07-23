81fbf25cde1227d6156479838ee646950c7ae024
Ana Beatriz Souza <anasouza@microsoft.com>
2018-07-23 16:14:13 -0700

    Update address test

--- identity-smart-contract/test/identity.js
+++ identity-smart-contract/test/identity.js
@@ -19,5 +19,44 @@ contract('Identity', function(accounts) {
     });
 });
 
+contract('Identity', function(accounts) {
+  
+    it("Should update the registration of Tenant with new account -part 1", function() {
+        return Identity.deployed().then(function(instance) {
+        
+            var tenantHash = web3.sha3('tenantId!');
+            var address = "0x47635c238f8af460e37e772387364ddd86c43a61";
+            var address2 = "0x47635c238f8af460e37e772387364ddd86c43a62";
+            var timestamp = 123123123
 
+            instance.setTenant(tenantHash, address1, timestamp);
+            instance.updateAddress(address1, address2);
 
+            return instance.userAddressExists.call(address1);
+
+        }).then(function(result) {
+            assert.equal(result, false);
+        });
+    });
+});
+
+contract('Identity', function(accounts) {
+  
+    it("Should update the registration of Tenant with new account -part 2", function() {
+        return Identity.deployed().then(function(instance) {
+        
+            var tenantHash = web3.sha3('tenantId!');
+            var address = "0x47635c238f8af460e37e772387364ddd86c43a61";
+            var address2 = "0x47635c238f8af460e37e772387364ddd86c43a62";
+            var timestamp = 123123123
+
+            instance.setTenant(tenantHash, address1, timestamp);
+            instance.updateAddress(address1, address2);
+
+            return instance.userAddressExists.call(address2);
+
+        }).then(function(result) {
+            assert.equal(result, true);
+        });
+    });
+});
