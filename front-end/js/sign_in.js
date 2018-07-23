832eca314d5adffd5e51f30bb69d46601712774d
Shawn Tabrizi <shawntabrizi@gmail.com>
2018-07-23 11:13:17 -0700

    update config

--- front-end/js/sign_in.js
+++ front-end/js/sign_in.js
@@ -1,5 +1,11 @@
 function sign_in() {
-    var userAgentApplication = new Msal.UserAgentApplication('79d908c3-6cc1-40c6-bbf1-9f7140e927fb', null, function (errorDes, token, error, tokenType, instance) {
+
+    var applicationConfig = {
+        clientID: '79d908c3-6cc1-40c6-bbf1-9f7140e927fb',
+        graphScopes: ["user.read"]
+    };
+
+    var userAgentApplication = new Msal.UserAgentApplication(applicationConfig.clientID, null, function (errorDes, token, error, tokenType, instance) {
         // this callback is called after loginRedirect OR acquireTokenRedirect. It's not used with loginPopup,  acquireTokenPopup.
         if (error) {
             console.log(error + ": " + errorDes);
@@ -9,18 +15,18 @@ function sign_in() {
 
     })
 
-    userAgentApplication.loginRedirect(["user.read"]).then(function (token) {
+    userAgentApplication.loginRedirect(applicationConfig.graphScopes).then(function (token) {
         var user = userAgentApplication.getUser();
         if (user) {
             console.log("signed in sucessfully");
 
             // get an access token
-            userAgentApplication.acquireTokenSilent(["user.read"]).then(function (token) {
+            userAgentApplication.acquireTokenSilent(applicationConfig.graphScopes).then(function (token) {
                 console.log("Success acquiring access token");
             }, function (error) {
                 // interaction required
                 if (error.indexOf("interaction_required" != -1)) {
-                    userAgentApplication.acquireTokenPopup(["user.read"]).then(function (token) {
+                    userAgentApplication.acquireTokenPopup(applicationConfig.graphScopes).then(function (token) {
                         console.log("Success acquiring access token");
                     }, function (error) {
                         console.log("Failure acquiring token: " + error);
