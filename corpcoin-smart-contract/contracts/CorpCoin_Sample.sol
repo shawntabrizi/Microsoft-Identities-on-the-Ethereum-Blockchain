31e2466f593316256ab63c213fab1932990d6074
azv32 <ashav23@gmail.com>
2018-07-24 14:10:39 -0700

    c

--- corpcoin-smart-contract/contracts/CorpCoin_Sample.sol
+++ corpcoin-smart-contract/contracts/CorpCoin_Sample.sol
@@ -30,7 +30,7 @@ contract CorpCoin is EIP20Interface {
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balances[msg.sender] >= _value);
-        require(idStore.hasAccountExpired(_to, expiration), "User not valid for transfer");
+        require(idStore.isValid(_to, block.timestamp - (expiration * 1 days)), "User not valid for transfer");
         balances[msg.sender] -= _value;
         balances[_to] += _value;
         emit Transfer(msg.sender, _to, _value); 
