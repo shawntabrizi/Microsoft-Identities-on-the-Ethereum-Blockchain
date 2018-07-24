320b142819fa646f6e535277262dac5bf1449a5d
fernandolobato <lobato.meeser.fernando@hotmail.com>
2018-07-23 18:08:53 -0700

    a

--- corpcoin-smart-contract/contracts/IdentityStoreInterface.sol
+++ corpcoin-smart-contract/contracts/IdentityStoreInterface.sol
@@ -1,8 +1,12 @@
 pragma solidity ^0.4.24;
 
 contract IdentityStore {
-    mapping(address => string) public tenantMapping;
     
-    function isAlive() public pure returns(bool alive) {}
+    function isValid(string _tenantId, address _userAddress, uint256 _minTimestamp) view public returns(bool){}
+
+    function userAddressExists(address userAddress) view public returns(bool){}
+
     function hasAccountExpired(address userAddress, uint validDays ) view public returns(bool){}
+
+    function userTenantHashExists(bytes32 tenantHash) view public returns(bool){}
 }
