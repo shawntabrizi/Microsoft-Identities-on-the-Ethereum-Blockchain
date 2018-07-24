eba2148cbfe052350b937cb6537db8ca9aff1bc0
hra <hamid.alipour@live.com>
2018-07-24 13:05:24 -0700

    add random pKey Genrator + move QR to a separate js script

--- front-end/js/create_eth_account.js
+++ front-end/js/create_eth_account.js
@@ -1,22 +1,68 @@
+function getRNG()
+{
+	//Webkit2's crazy invertible mapping generator
+	// Theory is here: http://dl.acm.org/citation.cfm?id=752741
+	// from https://bocoup.com/blog/random-numbers
+	var num_digits = 32;
+	var max = Math.pow(2, num_digits),
+		seed;
+	return {
+		setSeed : function(val) {
+			seed = val || Math.round(Math.random() * max);
+		},
+		getSeed : function() {
+			return seed;
+		},
+		rand : function() {
+			// creates randomness...somehow...
+			seed += (seed * seed) | 5;
+			// Shift off bits, discarding the sign. Discarding the sign is
+			// important because OR w/ 5 can give us + or - numbers.
+			return (seed >>> num_digits) / max;
+			//return Math.abs(seed);
+		}
+	};
+}
+
 function get_eth_account()
 {
+	var rng = getRNG();
+	rng.setSeed();
+	var pKey = "";
+	for (let i=0; i<8; i++)
+	{
+		pKey += rng.rand().toString(16).slice(2);
+	}
+	var hex_digits = 64;
+	var pad = hex_digits - pKey.length;
+	for (let i = 0; i < pad; i++)
+	{
+		pKey = '0' + pKey;
+	}
+	pKey = pKey.length > hex_digits ?  pKey.slice(0, hex_digits) : pKey;
+    console.log(pKey);
 	return {
-		address: "0xb8CE9ab6943e0eCED004cDe8e3bBed6568B2Fa01",
-    	privateKey: "0x348ce564d427a3311b6536bbcff9390d69395b06ed6c486954e971d960fe8709"
+		privateKey: pKey
 	};
 }
 
-function set_qr_account()
+function set_qr_account(account)
 {
-    qr_account = {
-		text : "0x348ce564d427a3311b6536bbcff9390d69395b06ed6c486954e971d960fe8709"
-	};
-
 	jQuery('#qrcodeAccount').qrcode({
-		text	: "0x348ce564d427a3311b6536bbcff9390d69395b06ed6c486954e971d960fe8709"
+		text	: account.privateKey
 	});	
-	console.log("creating account");
-	console.log(qr_account.text);
 }
 
-set_qr_account();
+$(document).ready( function() {
+	// code in here only runs when the page content is loaded
+	if(jQuery){
+		console.log('adding QR');
+
+		var eth_account = get_eth_account();
+		set_qr_account(eth_account);
+	}
+	else
+	{
+		console.log('failed to load jQuery');
+	}
+ });
