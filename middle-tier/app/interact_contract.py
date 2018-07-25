1008071bb3af0b9c7fe331a085032662ea4ff8c2
hongchao guan <hguan@microsoft.com>
2018-07-25 13:41:36 -0700

    add retry and nonce increase logic for signup

--- middle-tier/app/interact_contract.py
+++ middle-tier/app/interact_contract.py
@@ -14,12 +14,13 @@ CONTRACT_ADDRESS = environ.get('CONTRACT_ADDRESS')
 
 NETWORK_ENDPOINT = "https://ropsten.infura.io/v3/{}".format(API_KEY)
 
-
 w3 = Web3(HTTPProvider(NETWORK_ENDPOINT))
 w3.eth.enable_unaudited_features()
+#known_nonce = set()
 
 def setTenant(hashObject, address, timestamp, tenantId):
-    
+    #global known_nonce
+
     contract = load_contract()
     account = w3.eth.account.privateKeyToAccount(PRIVATE_KEY) 
 
@@ -32,21 +33,52 @@ def setTenant(hashObject, address, timestamp, tenantId):
             tenantId
         ])
 
-    transaction = {
-        'to': contract.address,
-        'data': get_data,
-        'gas': 1728712,
-        'gasPrice': w3.toWei('21', 'gwei'),
-        'nonce': w3.eth.getTransactionCount(account.address)
-    }
+    trans_count = w3.eth.getTransactionCount(account.address)
+    nonce = trans_count
+    #while nonce in known_nonce:
+    #    nonce += 1
 
-    signed = w3.eth.account.signTransaction(transaction, PRIVATE_KEY)
+    print("transaction count=%d nonce=%d" %(trans_count, nonce))
 
-    txn_hash = w3.eth.sendRawTransaction(signed.rawTransaction)
-    txn = w3.eth.getTransaction(txn_hash)
+    price = w3.toWei('21', 'gwei')
+    success = False
+    retry = 100
+    while not success and retry > 0:
+        retry -= 1
+        try:
+            transaction = {
+                'to': contract.address,
+                'data': get_data,
+                'gas': 1728712,
+                'gasPrice': price,
+                'nonce': nonce
+            }
 
-    print('Contract Transaction Hash {}'.format(txn_hash))
-    print('Transaction {}'.format(txn))
+            signed = w3.eth.account.signTransaction(transaction, PRIVATE_KEY)
+            txn_hash = w3.eth.sendRawTransaction(signed.rawTransaction)
+            txn = w3.eth.getTransaction(txn_hash)
+            print('Contract Transaction Hash {}'.format(txn_hash))
+            print('Transaction {}'.format(txn))
+
+            #known_nonce.add(nonce)
+            success = True
+        except ValueError as err:
+            err_msg = err.args[0]['message']
+            print('web3 error:: %s' % err_msg)
+            if 'replacement transaction underpriced' in err_msg:
+                price += 1
+                retry += 1 # underprice doesn't count for retrying
+                print('increase price to %d' % price)
+            elif 'nonce too low' in err_msg or 'known transaction' in err_msg:
+                #known_nonce.add(nonce)
+                nonce += 1
+                print('increase nonce to %d' % nonce)
+            else:
+                raise err
+
+    if retry <= 0:
+        print('stop retrying')
+
 
 def get_deloyed_contract(contract_definition, contract_address): 
     contract_abi = contract_definition['abi']
